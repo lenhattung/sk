fb0d61046a0422b51c973335b3b1ab31af6d777a
lenhattung <49211930+lenhattung@users.noreply.github.com>
2020-10-02 14:56:31 +0200

    commit

--- views/profile/_form.php
+++ views/profile/_form.php
@@ -7,7 +7,7 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\Profile */
 /* @var $form yii\widgets\ActiveForm */
 
-$rels = \yii\helpers\ArrayHelper::map(\app\models\Relationship::findAll(), 'id', 'name')
+$rels = \yii\helpers\ArrayHelper::map((\app\models\Relationship::find()->all()), 'id', 'name');
 
 ?>
 <style>
